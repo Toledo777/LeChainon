6ed99bfad1897feef7a26e2a5fb9db2b91f5472f
iamjny <jjng435@gmail.com>
2024-03-22 23:43:32 -0400

    Added housing availability and top_tiles endpoints

---
+++ backend/housing/routes.py
@@ -0,0 +1,28 @@
+from flask import *
+from firebase.config import db
+
+housing_bp = Blueprint("housing", __name__, url_prefix="/housing")
+
+# Updating housing availability data
+@housing_bp.route("/update-availability", methods=["POST"])
+def update_availability():
+    data={
+        "available_space": (request.form.get("available_space")),
+        "avg_length_stay": (request.form.get("avg_length_stay")),
+        "occupation_rate": (request.form.get("occupation_rate"))
+    }
+    db.collection("housing").add(data)
+    return jsonify({"message": "Success!"}), 200
+
+# Fetching housing availability data 
+@housing_bp.route("/get-housing-data", method=["GET"])
+def get_housing_data():
+    docs_ref = db.collection("housing").stream()
+    housing_data = []
+
+    for doc in docs_ref:
+        housing_data.append(doc.to_dict())
+
+    return jsonify({"message": "Success!", "housing_data": housing_data}), 200
+
+
---
+++ backend/top_tiles/routes.py
@@ -0,0 +1,26 @@
+from flask import *
+from firebase.config import db
+
+top_tiles_bp = Blueprint("top_tiles", __name__, url_prefix="/top_tiles")
+
+# Updating top_tiles data
+@top_tiles_bp.route("/create_top_tiles", methods=["POST"])
+def create_top_tiles():
+    data={
+        "c_r_ratio": (request.form.get("c_r_ratio")),
+        "total_caregivers": (request.form.get("total_caregivers")),
+        "total_residents": (request.form.get("total_residents"))
+    }
+    db.collection("top_tiles").add(data)
+    return jsonify({"message": "Success!"}), 200
+
+# Fetching top tiles data
+@top_tiles_bp.route("/get-top-tiles-data", method=["GET"])
+def get_top_tiles_data():
+    docs_ref = db.collection("top_tiles").stream()
+    top_tiles_data = []
+
+    for doc in docs_ref:
+        top_tiles_data.append(doc.to_dict())
+
+    return jsonify({"message": "Success!", "top_tiles_data": top_tiles_data}), 200

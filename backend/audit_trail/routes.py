649a8efae8806fdc7913f5c510fd8fd7ed1b451f
iamjny <jjng435@gmail.com>
2024-03-23 14:07:18 -0400

    Modified audit_trail endpoint

--- backend/audit_trail/routes.py
+++ backend/audit_trail/routes.py
@@ -6,20 +6,24 @@ audit_trail_bp = Blueprint("audit_trail", __name__, url_prefix="/audit_trail")
 # Create audit trail data
 @audit_trail_bp.route("/create", methods=["POST"])
 def create_audit_trail():
-    data={
-        "modifications_by_caregiver":{
-            "caregiver_id": request.form.get("caregiver_id"),
-        "item_modified": request.form.get("item_modified"),
-        "timestamp": request.form.get("timestamp")
-        },
-        "modifications_by_resident":{
-            "resident_id": request.form.get("caregiver_id"),
-        "item_modified": request.form.get("item_modified"),
-        "timestamp": request.form.get("timestamp")
-        }
+    caregiver_data = {
+        "caregiver_id": request.form.get("modifications_by_caregiver[caregiver_id]"),
+        "item_modified": request.form.get("modifications_by_caregiver[item_modified]"),
+        "timestamp": request.form.get("modifications_by_caregiver[timestamp]")
     }
-    db.collection("audit_trail").add(data)
-    return jsonify({"message": "Success!"}), 200
+
+    resident_data = {
+        "resident_id": request.form.get("modifications_by_resident[resident_id]"),
+        "item_modified": request.form.get("modifications_by_resident[item_modified]"),
+        "timestamp": request.form.get("modifications_by_resident[timestamp]")
+    }
+
+    db.collection("audit_trail").add({
+        "modifications_by_caregiver": caregiver_data,
+        "modifications_by_resident": resident_data
+    })
+
+    return jsonify({"message": "Success!", "caregiver_data": caregiver_data, "resident_data": resident_data}), 200
 
 # Fetch audit trail data
 @audit_trail_bp.route("/get-audit-trail", methods=["GET"])

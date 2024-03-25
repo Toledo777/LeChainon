24ee12f9a93141e141b29e589002eab31bb9d97e
Alice Nguyen <90719648+aintic@users.noreply.github.com>
2024-03-24 20:55:14 -0400

    add goal summary to resident view

---
+++ frontend/src/sections/overview/app-goal-summary.jsx
@@ -0,0 +1,100 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
+import CardHeader from '@mui/material/CardHeader';
+
+import { fShortenNumber } from 'src/utils/format-number';
+import Divider from '@mui/material/Divider';
+
+// ----------------------------------------------------------------------
+
+export default function AppGoalSummary({ title, list, onNewItem, ...other }) {
+
+  const [createNewItem, setCreateNewItem] = useState(false);
+
+  const handleCreateNewClick = () => {
+    setCreateNewItem(!createNewItem);
+  };
+
+  const handleFormSubmit = (item) => {
+    onNewItem(item);
+    setCreateNewItem(false);
+  };
+
+  return (
+    <Card {...other}>
+
+    <CardHeader title={title} />
+
+      <Box
+        sx={{
+          p: 3,
+          gap: 2,
+          display: 'grid',
+          gridTemplateColumns: 'repeat(3, 1fr)',
+        }}
+      >
+
+        {list.map((goal) => (
+          <Paper
+            key={goal.uid}
+            variant="outlined"
+            sx={{ py: 2.5, textAlign: 'left', borderStyle: 'dashed' }}
+          >
+            <Typography variant="h5">{goal.goal_title}</Typography>
+            <Typography variant="h6">Term: {goal.term}</Typography>
+            <Typography variant="h6">Status: {goal.status}</Typography>
+            <Typography variant="h6">Health aspects: {goal.health_aspects}</Typography>
+
+            <Typography variant="body1" sx={{ color: 'text.secondary' }}>
+              {goal.means}
+            </Typography>
+          </Paper>
+        ))}
+      </Box>
+      
+      {onNewItem && (
+          <>
+          <Divider sx={{ borderStyle: 'dashed' }} />
+
+          <Box sx={{ p: 2, textAlign: 'right' }}>
+            <Button
+              size="small"
+              color="inherit"
+              endIcon={<Iconify icon={createNewItem ? "eva:arrow-ios-downward-fill" : "eva:arrow-ios-forward-fill"} />}
+              onClick={handleCreateNewClick}
+            >
+              Create New
+            </Button>
+
+            {createNewItem && (
+              itemType === 'note' ?
+                <NewNoteForm
+                date={selectedDate}
+                onCancel={handleCreateNewClick} 
+                onSubmit={handleFormSubmit} 
+                /> 
+              :
+                <NewEventForm
+                date={selectedDate}
+                onCancel={handleCreateNewClick}
+                onSubmit={handleFormSubmit}
+                />
+            )}
+        </Box>
+        </>
+        )}
+
+    </Card>
+  );
+}
+
+AppGoalSummary.propTypes = {
+  title: PropTypes.string,  
+  subheader: PropTypes.string,
+  onNewItem: PropTypes.func,
+  list: PropTypes.array.isRequired,
+};
--- frontend/src/sections/overview/view/resident-view.jsx
+++ frontend/src/sections/overview/view/resident-view.jsx
@@ -12,6 +12,9 @@ import AppTasks from '../app-tasks';
 import AppNewsUpdate from '../app-news-update';
 import EventsTimeline from '../app-events-timeline';
 import AppWidgetSummary from '../app-widget-summary';
+import AppItemsOfDay from '../app-items-by-date';
+import AppGoalStats from '../app-goal-stats';
+import AppGoalSummary from '../app-goal-summary';
 
 // ----------------------------------------------------------------------
 
@@ -20,7 +23,167 @@ export default function ResidentView() {
 
     /* eslint-disable no-unused-vars */
 
-    const [value, onChange] = useState(new Date());
+    const [selectedDate, setSelectedDate] = useState(new Date());
+
+    const [tasks, setTasks] = useState([
+      {
+        id : 1,
+        details: "Read a book on quitting cigarettes",
+      },
+      {
+        id : 2,
+        details: "Read a book on quitting alcohol",
+      },
+      {
+        id : 3,
+        details: "Read a book on Happiness",
+      },
+      {
+        id : 4,
+        details: " Try eating healthy",
+      },
+      
+      {
+        id : 5,
+        details: "Read a book on quitting gambaling ",
+      },
+      {
+        id : 6,
+        details: "Start learning Yoga",
+      }
+    ]);
+
+    const [treatmentTeam, setTreatmentTeam] = useState([
+      {
+        name: "Dr. Beau Gessel",
+        role: "Psychologist",
+        phone: "555-555-5555",
+        email: "abc@gmail.com",
+        address: "4865 St Laurent Blvd, Montreal, QC H2W 1Z8"
+      },
+      {
+        name: "Dr. Kimberly Bertotti",
+        role: "Therapist",
+        phone: "777-777-7777",
+        email: "def@gmail.com",
+        address: "4701 Rue Saint-Urbain, Montreal, QC H2T 2V9"
+      },
+      {
+        name: "Dr. Anita Figueras",
+        role: "Social Worker",
+        phone: "999-999-9999",
+        email: "ghi@gmail.com",
+        address: "1045 Av. Laurier O, Outremont, QC H2V 2L1"
+      },
+      {
+        name: "Dr. Christian Scriver",
+        role: "MD (specialty)",
+        phone: "000-000-0000",
+        email: "jkl@gmail.com",
+        address: "9187 Bd de l'Acadie, Montreal, QC H4N 3K1"
+      }
+    ]);
+
+    const [goals, setGoals] = useState([
+      {
+        uid: 1,
+        goal_title: "Greatly Reduce Alcohol Consumption and Quit Smoking",
+        health_aspects: "Mental",
+        means: "Become aware of alcohol consumption and risk factors that lead to it",
+        status: "In progress",
+        term: "Short"
+      },
+      {
+        uid: 2,
+        goal_title: "Finding Apartment/Affordable Housing",
+        health_aspects: "Economic Health",
+        means: "Determine which organization or resources she could use to access housing with her income.",
+        status: "Future",
+        term: "Long"
+      },
+      {
+        uid: 3,
+        goal_title: "Start Hockey Training and Join Team",
+        health_aspects: "Physical",
+        means: "Conduct research related to sportts activities. Obtain sources of funding if possible.",
+        status: "Completed",
+        term: "Medium"
+      }
+    ]);
+
+    const [significantPersons, setSignificantPersons] = useState([
+      {
+        relation: "Mother",
+        name: "John Doe",
+        phone: "555-555-5555",
+        email: "abc@gmail.com"
+      },
+      {
+        relation: "Daughter",
+        name: "Jason D",
+        phone: "555-522-5555",
+        email: "pqr@gmail.com"
+      },
+      {
+        relation: "Sister",
+        name: "Janna Rachel",
+        phone: "555-555-5523",
+        email: "qwe@gmail.com"
+      }
+      
+    ])
+
+    const [communityServices, setCommunityServices] = useState([
+      {
+        org_name: 'The Shield of Montreal',
+        contact: 'Fransis Coles',
+        phone: '541-333-234',
+        email: 'abc@gmail.com',
+        mission: 'Providing a safe and empowering environment',
+      },
+      {
+        org_name: 'The Shield of Mont Royal',
+        contact: 'Ranndy',
+        phone: '233-456-1234',
+        email: 'qwr@gmail.com',
+        mission: 'Providing protection to refugees',
+      },
+      {
+        org_name: 'West Island Women Shelter',
+        contact: 'John Matthews',
+        phone: '234-344-2344',
+        email: 'pqr@gmail.com',
+        mission: 'Providing protection to refugees',
+      }
+    ])
+
+    const [events, setEvents] = useState([
+      {
+        title: 'crisis intervention 1',
+        follow_up_date: 'Fri, 22 Mar 2024 04:00:00 GMT',
+        communication_method: 'In person',
+        type: 'appointment',
+        notes: 'Resident was in a crisis situation and needed immediate intervention.',
+        resident: 'Jane Doe'
+      },
+      {
+        title: 'follow up 1',
+        follow_up_date: 'Fri, 15 Mar 2024 12:00:00 GMT',
+        communication_method: 'Email',
+        type: 'checkpoint',
+        notes: 'Routine check up. Resident is progressing well.',
+        resident: 'Jane Doe'
+      },
+      {
+        title: 'follow up 2',
+        follow_up_date: 'Fri, 29 Mar 2024 04:00:00 GMT',
+        communication_method: 'In person',
+        type: 'checkpoint',
+        notes: 'Routine check up. Active listening and helped client with employment issue.',
+        resident: 'Jane Doe'
+      }
+    ])
+
     const state = {
       'date': '04-03-2024'
     }
@@ -74,214 +237,87 @@ export default function ResidentView() {
           />
         </Grid>
 
-        {/* <Grid xs={12} md={6} lg={8}>
-          <AppWebsiteVisits
-            title="Website Visits"
-            subheader="(+43%) than last year"
-            chart={{
-              labels: [
-                '01/01/2003',
-                '02/01/2003',
-                '03/01/2003',
-                '04/01/2003',
-                '05/01/2003',
-                '06/01/2003',
-                '07/01/2003',
-                '08/01/2003',
-                '09/01/2003',
-                '10/01/2003',
-                '11/01/2003',
-              ],
-              series: [
-                {
-                  name: 'Team A',
-                  type: 'column',
-                  fill: 'solid',
-                  data: [23, 11, 22, 27, 13, 22, 37, 21, 44, 22, 30],
-                },
-                {
-                  name: 'Team B',
-                  type: 'area',
-                  fill: 'gradient',
-                  data: [44, 55, 41, 67, 22, 43, 21, 41, 56, 27, 43],
-                },
-                {
-                  name: 'Team C',
-                  type: 'line',
-                  fill: 'solid',
-                  data: [30, 25, 36, 30, 45, 35, 64, 52, 59, 36, 39],
-                },
-              ],
-            }}
+        <Grid xs={12} md={6} lg={12}>
+          <AppGoalSummary
+            title="Goal Summary"
+            list={goals}
+            onNewItem={(item) => setGoals((prev) => [...prev, item])}
           />
-        </Grid> */}
+        </Grid>
 
-        <Grid xs={12} md={6} lg={4}>
+        <Grid xs={12} md={6} lg={4} sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
           <Calendar
-            style={{ height: 500 }}
-            onChange={onChange}
-            value={value}
+            style={{ height: 800, width: 800}}
+            onChange={setSelectedDate}
+            value={selectedDate}
             // eslint-disable-next-line consistent-return
             tileClassName={({ date, view }) => {
                 if(mark.find(x => x === moment(date).format("DD-MM-YYYY"))) {
                 return  'highlight'
               }
             }}
-              tileDisabled={({ date }) => date.getDay() === 0}
-              minDate={
-                new Date()
-              }
             />
         </Grid>
 
-        {/* <Grid xs={12} md={6} lg={8}>
-          <AppConversionRates
-            title="Conversion Rates"
-            subheader="(+43%) than last year"
-            chart={{
-              series: [
-                { label: 'Italy', value: 400 },
-                { label: 'Japan', value: 430 },
-                { label: 'China', value: 448 },
-                { label: 'Canada', value: 470 },
-                { label: 'France', value: 540 },
-                { label: 'Germany', value: 580 },
-                { label: 'South Korea', value: 690 },
-                { label: 'Netherlands', value: 1100 },
-                { label: 'United States', value: 1200 },
-                { label: 'United Kingdom', value: 1380 },
-              ],
-            }}
-          />
-        </Grid> */}
-
-        {/* <Grid xs={12} md={6} lg={4}>
-          <AppCurrentSubject
-            title="Current Subject"
-            chart={{
-              categories: ['English', 'History', 'Physics', 'Geography', 'Chinese', 'Math'],
-              series: [
-                { name: 'Series 1', data: [80, 50, 30, 40, 100, 20] },
-                { name: 'Series 2', data: [20, 30, 40, 80, 20, 80] },
-                { name: 'Series 3', data: [44, 76, 78, 13, 43, 10] },
-              ],
-            }}
-          />
-        </Grid> */}
-
-        {/* <Grid xs={12} md={6} lg={8}>
-          <AppNewsUpdate
-            title="News Update"
-            list={[...Array(5)].map((_, index) => ({
-              id: faker.string.uuid(),
-              title: faker.person.jobTitle(),
-              description: faker.commerce.productDescription(),
-              image: `/assets/images/covers/cover_${index + 1}.jpg`,
-              postedAt: faker.date.recent(),
-            }))}
-          />
-        </Grid> */}
-
         <Grid xs={12} md={6} lg={4}>
           <EventsTimeline
-            title="Finding an Appartment / Affordable Housing"
-            list={[...Array(2)].map((_, index) => ({
-              id: faker.string.uuid(),
-              title: [
-                'Find organizations to help acess affordable housing',
-                'Find resources to access affordable housing',
-              ][index],
-              type: `order${index + 1}`,
-              time: '',
+            title="Upcoming Events"
+            list={events
+              .filter(event => new Date(event.follow_up_date) > new Date())
+              .sort((a, b) => new Date(b.follow_up_date) - new Date(a.follow_up_date))
+              .slice(0, 3)
+              .map(event => ({
+                id: event.uid,
+                title: `${event.resident}, ${event.title}, ${event.communication_method}`,
+                type: event.type,
+                time: new Date(event.follow_up_date),
             }))}
           />
         </Grid>
 
         <Grid xs={12} md={6} lg={4}>
           <EventsTimeline
-            title="Greatly reduce alcohol consumption and quit Smoking"
-            list={[...Array(2)].map((_, index) => ({
-              id: faker.string.uuid(),
-              title: [
-                'Leave alcohol in the office',
-                'Become of aware or alcohol consuption and risk factors that lead to it',
-              ][index],
-              type: `order${index + 1}`,
-              time: '',
+            title="Recent Events"
+            list={events
+              .filter(event => new Date(event.follow_up_date) <= new Date())
+              .sort((a, b) => new Date(b.follow_up_date) - new Date(a.follow_up_date))
+              .slice(0, 3)
+              .map(event => ({
+                id: event.uid,
+                title: `${event.resident}, ${event.title}, ${event.communication_method}`,
+                type: event.type,
+                time: new Date(event.follow_up_date),
             }))}
           />
         </Grid>
-
+        
         <Grid xs={12} md={6} lg={4}>
-          <EventsTimeline
-            title="Payment of Tickets, Fines and Debts"
-            list={[...Array(2)].map((_, index) => ({
-              id: faker.string.uuid(),
-              title: [
-                'Make arrangments with various authorities with debt',
-                'Take steps to find info on the total prices',
-
-              ][index],
-              type: `order${index + 1}`,
-              time: '',
-            }))}
+          <AppItemsOfDay
+            title={`Events (${moment(selectedDate).format('DD/MM/YYYY')})`}
+            itemType="event"
+            selectedDate={selectedDate}
+            list={
+              events
+              .filter(event => moment(event.follow_up_date).isSame(selectedDate, 'day'))
+              .map(event => ({
+                title: event.title,
+                date: new Date(event.follow_up_date),
+                notes: event.notes,
+                resident: event.resident,
+                type: event.type,
+                communication_method: event.communication_method
+              }))
+            }
           />
         </Grid>
-
-        <Grid xs={12} md={6} lg={8}>
-          <EventsTimeline
-            title="Start Hockey Training and Join Team"
-            list={[...Array(3)].map((_, index) => ({
-              id: faker.string.uuid(),
-              title: [
-                'Approach sports organizations to obtain necessary equipment',
-                'Obtain sources of funding if possible',
-                'Conduct research related to sports activities',
-              ][index],
-              type: `order${index + 1}`,
-              time: '',
-            }))}
-          />
-        </Grid>
-
-        {/* <Grid xs={12} md={6} lg={4}>
-          <AppTrafficBySite
-            title="Traffic by Site"
-            list={[
-              {
-                name: 'FaceBook',
-                value: 323234,
-                icon: <Iconify icon="eva:facebook-fill" color="#1877F2" width={32} />,
-              },
-              {
-                name: 'Google',
-                value: 341212,
-                icon: <Iconify icon="eva:google-fill" color="#DF3E30" width={32} />,
-              },
-              {
-                name: 'Linkedin',
-                value: 411213,
-                icon: <Iconify icon="eva:linkedin-fill" color="#006097" width={32} />,
-              },
-              {
-                name: 'Twitter',
-                value: 443232,
-                icon: <Iconify icon="eva:twitter-fill" color="#1C9CEA" width={32} />,
-              },
-            ]}
-          />
-        </Grid> */}
 
         <Grid xs={12} md={6} lg={8}>
           <AppTasks
             title="Goal Task Tracking"
-            list={[
-              { id: '1', name: 'Read a book on quitting cigarettes' },
-              { id: '2', name: 'Register for house activities: dance, sports, yoga' },
-              { id: '3', name: 'Sign up for legal clinic' },
-              { id: '4', name: 'Make appointment with legal clinic' },
-       
-            ]}
+            list={tasks.map((task) => ({
+              id: task.id,
+              name: task.details,
+            }))}
           />
         </Grid>
       </Grid>
